ea24b8fb30aebf24aea77d8c119f22a30ac871a2
Eugene <Eugeniolio@yandex.com>
2020-01-04 23:21:27 +0300

    Add git ignore

--- app.js
+++ app.js
@@ -182,6 +182,6 @@ app.get("/:param", function(req,res){
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
